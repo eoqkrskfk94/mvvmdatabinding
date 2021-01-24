45a6ddd1d7240799ac0bc9dc68ca7b173167097c
김명진 <mobinity.kmj@gmail.com>
2021-01-24 14:15:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mvvmdatabinding/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.mvvmdatabinding
+
+class MainViewModel {
+    public lateinit var title: String;
+
+    val user = User("Example")
+
+
+}
---
+++ app/src/main/java/com/example/mvvmdatabinding/User.kt
@@ -0,0 +1,5 @@
+package com.example.mvvmdatabinding
+
+data class User(var title: String) {
+
+}
